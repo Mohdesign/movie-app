58fded23488e3a88b2352ab2f889ed15fcbf7ca2
unknown <m_k_abdelkader@hotmail.com>
2020-12-04 20:17:51 -0400

    Add existing file

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
+
+
 module.exports = {
-	async redirects() {
-		return [
-			{
-				source: '/team',
-				destination: 'https://movie-app-lake-alpha.vercel.app',
-			},
-			{
-				source: '/about',
-				destination: '/',
-			},
-			// Path Matching - will match `/post/a` but not `/post/a/b`
-			{
-				source: '/post/:slug',
-				destination: '/news/:slug',
-			},
-			// Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
-			{
-				source: '/blog/:slug*',
-				destination: '/news/:slug*',
-			},
-			// Rewriting to an external URL
-			{
-				source: '/docs/:slug',
-				destination: 'http://example.com/docs/:slug',
-			},
-		]
+	experimental: {
+		async redirects() {
+			return [
+				{ source: '/team', destination: '/https://movie-app-lake-alpha.vercel.app', permanent: true }, // a permanent redirect
+			];
+		},
 	},
-}
+};
